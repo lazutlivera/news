1b68245e2f2a8260b633ccbeed7b374d2f26c76e
lazutlivera <uk.doganozturk@gmail.com>
2024-08-29 14:01:50 +0100

    this commit will include task 10(get-users) and tests

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -7,7 +7,8 @@ const {
   fetchComments,
   addComment,
   updateArticleVotes,
-  deleteCommentById
+  deleteCommentById,
+  fetchUsers,
 } = require("../models/topics.model");
 
 exports.getTopics = async (req, res, next) => {
@@ -47,44 +48,47 @@ exports.getCommentsByArticleId = async (req, res, next) => {
     const { article_id } = req.params;
     const comments = await fetchComments(article_id);
     res.status(200).send({ comments });
-  } catch(err) {
-    next(err)
+  } catch (err) {
+    next(err);
   }
 };
-exports.postComment = async (req, res, next) =>{
+exports.postComment = async (req, res, next) => {
   const { article_id } = req.params;
-  const {username, body} = req.body
+  const { username, body } = req.body;
 
-
-  try{
-    const comment = await addComment(article_id, username, body)
-    res.status(201).send({comment})
-  }catch(err){
-    next(err)
+  try {
+    const comment = await addComment(article_id, username, body);
+    res.status(201).send({ comment });
+  } catch (err) {
+    next(err);
   }
-}
+};
 exports.patchArticle = async (req, res, next) => {
   const { article_id } = req.params;
   const { inc_votes } = req.body;
 
   try {
-      if (typeof inc_votes !== 'number') {
-          throw { status: 400, msg: 'Invalid data type' };
-      }
+    if (typeof inc_votes !== "number") {
+      throw { status: 400, msg: "Invalid data type" };
+    }
 
-      const article = await updateArticleVotes(article_id, inc_votes);
-      res.status(200).send({ article });
+    const article = await updateArticleVotes(article_id, inc_votes);
+    res.status(200).send({ article });
   } catch (err) {
-      next(err);
+    next(err);
   }
 };
 exports.deleteComment = async (req, res, next) => {
   const { comment_id } = req.params;
 
   try {
-      await deleteCommentById(comment_id);
-      res.sendStatus(204);
+    await deleteCommentById(comment_id);
+    res.sendStatus(204);
   } catch (err) {
-      next(err);
+    next(err);
   }
+};
+exports.getUsers = async (req, res, next) => {
+  const users = await fetchUsers();
+  res.status(200).send({ users });
 };
